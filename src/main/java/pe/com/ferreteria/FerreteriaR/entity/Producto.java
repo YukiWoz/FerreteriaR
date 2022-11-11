3e3c3965c12ce8a7ca438d5ae2529c99df5c43b7
Jhovanyhs <jhovany.wilmer.huerta@gmail.com>
2022-11-11 01:06:03 -0500

    Update Producto.java

--- src/main/java/pe/com/ferreteria/FerreteriaR/entity/Producto.java
+++ src/main/java/pe/com/ferreteria/FerreteriaR/entity/Producto.java
@@ -1,8 +1,47 @@
 package pe.com.ferreteria.FerreteriaR.entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name="Producto")
+@Table(name="producto")
 public class Producto implements Serializable
 {
+
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @Column(name="codigopro")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long codigo;
+     @Column(name="nombrepro")
+    private String nombre;
+      @Column(name="preciopro ")
+    private double preciocompra;
+       @Column(name="precioventapro")
+    private double precioventa;
+        @Column(name="cantidadpro")
+    private double cantidad;
+         @Column(name="estadopro")
+    private boolean estado;
+         @ManyToOne
+         @JoinColumn(name="codigocat",nullable=false)
+    private Categoria categoria;
 
 }
