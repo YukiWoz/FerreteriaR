2bbc688710a9a20cd88507fcac217f1bab2246b9
JuniorCerpe <juniorcervera13@gmail.com>
2022-11-10 23:36:10 -0500

    Update Categoria.java

--- src/main/java/pe/com/ferreteria/FerreteriaR/entity/Categoria.java
+++ src/main/java/pe/com/ferreteria/FerreteriaR/entity/Categoria.java
@@ -2,7 +2,38 @@
 package pe.com.ferreteria.FerreteriaR.entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity( name="Categoria")
+@Table(name= "categoria")
 public class Categoria  implements Serializable{
 
+   private static final long serialVersionUID=1L;
+
+    @Id
+    @Column(name="codigocat")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long codigo;
+
+     @Column(name="nombrecat")
+    private String nombre;
+
+     @Column(name="estadocat")
+    private boolean estado;
+
 }
+
