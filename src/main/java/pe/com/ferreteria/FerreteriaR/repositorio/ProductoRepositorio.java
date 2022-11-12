972964a61b44e3934db348fe77a70d24cd212b85
YukiWoz <jake.uwu01@gmail.com>
2022-11-12 11:23:14 -0500

    Update ProductoRepositorio.java

--- src/main/java/pe/com/ferreteria/FerreteriaR/repositorio/ProductoRepositorio.java
+++ src/main/java/pe/com/ferreteria/FerreteriaR/repositorio/ProductoRepositorio.java
@@ -1,6 +1,16 @@
-
 package pe.com.ferreteria.FerreteriaR.repositorio;
 
 public interface ProductoRepositorio{
     
+}
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pe.com.ferreteria.FerreteriaR.entity.Producto;
+
+@Repository
+public interface ProductoRepositorio extends JpaRepository<Producto, Long>{
+    @Query("select p from Producto p where p.estado='1'")
+    List<Producto>findAllCustom();
 }
