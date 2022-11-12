ebcfba211ef3b13637735b9655285da245c63b70
Jhovanyhs <jhovany.wilmer.huerta@gmail.com>
2022-11-12 11:43:30 -0500

    Actu-producto Service
    Producto service
    
    -tocto mi mentor xd

--- src/main/java/pe/com/ferreteria/FerreteriaR/service/ProductoService.java
+++ src/main/java/pe/com/ferreteria/FerreteriaR/service/ProductoService.java
@@ -1,7 +1,20 @@
-
 package pe.com.ferreteria.FerreteriaR.service;
+
+import java.util.List;
+import java.util.Optional;
+import pe.com.ferreteria.FerreteriaR.entity.Producto;
 
 
 public interface ProductoService {
+    public List<Producto> findAll();
 
+    public List<Producto> findAllCustom();
+
+    public Optional<Producto> findById(long id);
+
+    public Producto add(Producto p);
+
+    public Producto update(Producto p);
+
+    public Producto delete(Producto p);
 }
