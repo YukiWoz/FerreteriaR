79009ff8fb42711a4296dcc29b00f443eb9a58d8
YukiWoz <jake.uwu01@gmail.com>
2022-11-16 23:35:07 -0500

    Update FerreteriaRApplicationTests.java

--- src/test/java/pe/com/ferreteria/FerreteriaR/FerreteriaRApplicationTests.java
+++ src/test/java/pe/com/ferreteria/FerreteriaR/FerreteriaRApplicationTests.java
@@ -1,5 +1,13 @@
 package pe.com.ferreteria.FerreteriaR;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class FerreteriaRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
